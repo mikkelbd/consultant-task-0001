9aa07bec77135dfe7ba6424193bd6ad7dba1e39b
Mikkel Dan-Rognlie <mikkel@decorus.dk>
2020-06-30 01:19:46 +0200

    Add POJOs to represent a json file with a list of users

---
+++ src/main/java/no/unit/transformer/User.java
@@ -0,0 +1,43 @@
+package no.unit.transformer;
+
+public class User {
+    private String name;
+    private int sequence;
+    private String role;
+    private String began;
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getBegan() {
+        return began;
+    }
+
+    public void setBegan(String began) {
+        this.began = began;
+    }
+}
