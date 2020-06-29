d6a4e64ad9aa5fde2adf50b3288c6c359432a7c7
Mikkel Dan-Rognlie <mikkel@decorus.dk>
2020-06-29 23:03:32 +0200

    Simplify requirements and make the transformer convert only list of user
    In its original version the requirements were kind of confusing. It seemed
    there were two tasks. One was to convert from e.g. JSON to XML and vice
    versa. The other was to change the format or representation of the user
    by parsing the name field and splitting it into a nested object with
    separate fields for given name and family name. It also had
    
    Furthermore the converter had to convert both a single object in an unnamed
    array, whereas the other use case was to convert a list of user objects
    nested under a "users" object. This just complicated things in this MVP.
    
    Now the requirements are simpler and hence the implementation can be
    simpler in its first version. Also, the spec can now describe in one
    Scenario outline the four combinations of input and output formats with
    one example table.
    
    The original requirement to order the elements by "sequence" in the
    exported file is retained.

--- src/test/java/no/unit/transformer/features/StepDefinitions.java
+++ src/test/java/no/unit/transformer/features/StepDefinitions.java
@@ -11,11 +11,9 @@ import picocli.CommandLine;
 import java.io.File;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StepDefinitions extends TestWiring {
 
-    public static final String SINGLE_OBJECT_JSON = "single_object.json";
     private Transformer transformer;
     private CommandLine commandLine;
 
@@ -50,65 +48,17 @@ public class StepDefinitions extends TestWiring {
     }
 
     @Given("the user has a file {string}")
-    public void theUserHasAFile(String filename) {
-        File actual = getFileFromResources(filename);
-        assertTrue(actual.exists());
-    }
-
-    @Given("the user has an input file that contains an array that contains a single object")
-    public void theUserHasAnInputFileThatContainsAnArrayThatContainsASingleObject() {
-        File actual = getFileFromResources(SINGLE_OBJECT_JSON);
-        assertTrue(actual.exists());
-    }
-
-    @And("the object has field {string} with string value {string}")
-    public void theObjectHasFieldWithStringValue(String field, String value) {
-        throw new PendingException();
-    }
-
-    @When("the user transforms the data")
-    public void theUserTransformsTheData() {
-        throw new PendingException();
-    }
-
-    @Then("the user sees that the output file contains an array that contains a single object")
-    public void theUserSeesThatTheOutputFileContainsAnArrayThatContainsASingleObject() {
-        throw new PendingException();
-    }
-
-    @And("the object has a field {string} with an integer value {string}")
-    public void theObjectHasAFieldWithAnIntegerValue(String field, String integer) {
-        throw new PendingException();
-    }
-
-    @And("the object has a field {string}")
-    public void theObjectHasAField(String field) {
-        throw new PendingException();
-    }
-
-    @And("the field \"identity\" contains an object with the fields \"given\" and \"family\"")
-    public void theFieldContainsAnObjectWithTheFieldsAnd() {
-        throw new PendingException();
-    }
-
-    @And("the field {string} contains string value {string}")
-    public void theFieldContainsStringValue(String field, String value) {
+    public void theUserHasAFileInputFile(String file) {
         throw new PendingException();
     }
 
     @And("the data is formatted correctly")
     public void theDataIsFormattedCorrectly() {
-        throw new PendingException();
+        // do nothing
     }
 
-    @When("the user transforms the file from (.*) to (.*)")
-    public void theUserTransformsTheFileFromSerializationAToSerializationB(String serializationA,
-                                                                           String serializatiionB) {
-        throw new PendingException();
-    }
-
-    @And("they open the file")
-    public void theyOpenTheFile() {
+    @When("the user transforms the file to (.*)")
+    public void theUserTransformsTheFileTo(String outputFile) {
         throw new PendingException();
     }
 
@@ -122,28 +72,8 @@ public class StepDefinitions extends TestWiring {
         throw new PendingException();
     }
 
-    @Given("the user has a (.*) in (.*)")
-    public void theUserHasAFileInSerialization(String filename, String serialization) {
-        throw new PendingException();
-    }
-
-    @When("they transform the file without specifying the output format flag")
-    public void theyTransformTheFileWithoutSpecifyingTheOutputFormatFlag() {
-        throw new PendingException();
-    }
-
     @And("they open the transformed file")
     public void theyOpenTheTransformedFile() {
-        throw new PendingException();
-    }
-
-    @Then("the file is transformed to serialization")
-    public void theFileIsTransformedToSerialization() {
-        throw new PendingException();
-    }
-
-    @And("the data is formatted badly")
-    public void theDataIsFormattedBadly() {
         throw new PendingException();
     }
 
@@ -152,8 +82,15 @@ public class StepDefinitions extends TestWiring {
         throw new PendingException();
     }
 
+    @Given("the user has a badly formatted file")
+    public void theUserHasABadlyFormattedFile() {
+        File file = getFileFromResources("badly_formatted.json");
+        assertThat(file).exists();
+    }
+
     @Then("they see an error message telling them that the input file is badly formatted")
     public void theySeeAnErrorMessageTellingThemThatTheInputFileIsBadlyFormatted() {
         throw new PendingException();
     }
+
 }
--- src/test/resources/features/user_transforms_file.feature
+++ src/test/resources/features/user_transforms_file.feature
@@ -9,53 +9,23 @@ Feature:
     And "Transformer" has a flag "--output-format" that takes a single argument "xml" or "json"
 
   @wip
-  Scenario: User transforms a single object from format A to format B
-    Given the user has an input file that contains an array that contains a single object
-    And the object has field "name" with string value "Carstens, Ull"
-    And the object has field "sequence" with string value "1"
-    And the object has field "role" with string value "Executive"
-    When the user transforms the data
-    Then the user sees that the output file contains an array that contains a single object
-    And the object has a field "id" with an integer value "1"
-    And the object has a field "identity"
-    And the field "identity" contains an object with the fields "given" and "family"
-    And the field "given" contains string value "Ull"
-    And the field "family" contains string value "Carstens"
-    And the object has field "role" with string value "Executive"
-
-  @wip
-  Scenario Outline: User transforms data from file to JSON
-    Given the user has a file "input.json"
+  Scenario Outline: User transforms a file with users from an input file to an output file
+    Given the user has a file "<input-file>"
     And the data is formatted correctly
-    When the user transforms the file from <serialization-a> to <serialization-b>
-    And they open the file
-    Then they see that the data is transformed to <serialization-b>
-    And that the elements in "users" section of the file are ordered by element "sequence"
-
-    Examples:
-      | serialization-a | serialization-b |
-      | xml             | xml             |
-      | json            | json            |
-      | xml             | json            |
-      | json            | xml             |
-
-  @wip
-  Scenario Outline: User transforms file without specifying an output format
-    Given the user has a <file> in <serialization>
-    And the data is formatted correctly
-    When they transform the file without specifying the output format flag
+    When the user transforms the file to <output-file>
     And they open the transformed file
-    Then the file is transformed to serialization
+    Then they see that the data is transformed to <output-format>
     And that the elements in "users" section of the file are ordered by element "sequence"
 
     Examples:
-      | file       | serialization |
-      | input.xml  | xml           |
-      | input.json | json          |
+      | input-file | output-file            | output-format |
+      | users.json | transformed_users.json | json          |
+      | users.xml  | transformed_users.xml  | xml           |
+      | users.json | transformed_users.xml  | xml           |
+      | users.xml  | transformed_users.json | json          |
 
   @wip
   Scenario: User attempts to transform badly formatted file
-    Given the user has a file "badly_formatted.json"
-    And the data is formatted badly
+    Given the user has a badly formatted file
     When the user attempts to transform the file
     Then they see an error message telling them that the input file is badly formatted
