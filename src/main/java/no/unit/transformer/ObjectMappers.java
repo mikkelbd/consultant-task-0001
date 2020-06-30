abcb53ab31e5a1e44f5413407741c2853ec896ee
Mikkel Dan-Rognlie <mikkel@decorus.dk>
2020-06-30 02:03:43 +0200

    Configurere ObjectMapper with output indentation

--- src/main/java/no/unit/transformer/ObjectMappers.java
+++ src/main/java/no/unit/transformer/ObjectMappers.java
@@ -1,10 +1,20 @@
 package no.unit.transformer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.File;
+import java.io.IOException;
+
 
 public final class ObjectMappers {
 
-    public static final ObjectMapper jsonObjectMapper = new ObjectMapper();
+    public static final ObjectMapper jsonObjectMapper = createJsonObjectMapper();
+
+    private static ObjectMapper createJsonObjectMapper() {
+        return new ObjectMapper()
+                .enable(SerializationFeature.INDENT_OUTPUT);
+    }
 
     private ObjectMappers() {
     }
